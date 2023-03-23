8ff8ca3098bcb37f91feda97e1503e6e88159433
rajkoladiya <rajesh.koladiya19@gmail.com>
2023-03-23 11:59:47 +0530

    First Part

--- force-app/main/default/lwc/editQuote/editQuote.js
+++ force-app/main/default/lwc/editQuote/editQuote.js
@@ -3,14 +3,66 @@
  * Copyright (c) 2023 Provus Inc. All rights reserved.
  */
 
-import { LightningElement, api } from "lwc";
+import { LightningElement, api , wire} from "lwc";
+
+import getRecord from '@salesforce/apex/QuoteDto.getRecord';
+import saveQuote from '@salesforce/apex/QuoteDto.saveQuote';
+import {refreshApex} from '@salesforce/apex';
+
+import {ShowToastEvent} from "lightning/platformShowToastEvent";
 
 export default class EditQuote extends LightningElement {
-  @api recordId;
-  quoteData = {
-    name: "Quote Name",
-    endDate: 1547250828000
-  };
+  @api recordId; quoteData; isLoaded = false;
 
+  get startDate(){
+    return this._startDate? this._startDate : this.quoteData.startDate;
+  }
+
+  get endDate(){
+    return this._endDate? this._endDate : this.quoteData._endDate;
+  }
+
+  changeStartHandle(event){
+    this._startDate = event.target.value;
+  }
+
+  changeEndHandle(event){
+    this._endDate = event.target.value;
+  }
+
+
+  saveDetails(){
+    saveQuote({
+      recordId : this.recordId,
+      startDate : startDate,
+      endDate : endDate
+    })
+    .then(() =>{
+      const toastEvent = new ShowToastEvent({
+        title : 'Success',
+        message : 'Quote updated Successfully!',
+        variant : 'success'
+      });
+
+      refreshApex(this.wiredGetRecord);
+    })
+    .catch((err) =>{
+      console.log("#error..");
+    });
+  }
+  
   renderedCallback() {}
+
+  @wire (getRecord , { recordId : "$recordId"})
+  wiredGetRecord({error, data}){
+    if(data){ 
+      this.quoteData = data;
+      this.isLoaded = true;
+    }else if(error){
+      console.log('##Error ');
+    }
+  }
+
+  
+
 }
